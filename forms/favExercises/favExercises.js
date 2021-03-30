1843b273a6d36eb23d14f2b3c7eb0902ef52855d
Connor Watley <62679869+Connor-Watley@users.noreply.github.com>
2021-03-30 17:52:04 -0500

    Update favExercises.js

--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -8,14 +8,21 @@ favExercises.onshow = function() {
         selExercises.addItem(exercises[i])
     for (j = 0; j < coreExercises.length; j++)
         selExercises.addItem(coreExercises[j])
-        
-        
+
+
 
 }
-Button3.onclick=function(){
- results =  selExercises.text.includes("situps", "plank")
- console.log(results)
- if(results == true){
-    Label1.value = `You chose situps and planks - those are the two core exercises. `}
-} else{
-    Label1.value = ``}
+Button3.onclick = function() {
+    results = selExercises.text.includes("situps", "plank")
+    console.log(results)
+    if (results == true) {
+        Label1.value = `You chose situps and planks - those are the two core exercises. `
+    } else {
+        Label1.value = ``
+    }
+}
+
+
+Button4.onclick = function() {
+ChangeForm(mobileNav)
+}
